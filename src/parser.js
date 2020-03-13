ec0028f968b010bc8d27e6c262c8ae25f3dcc4b5
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2020-03-13 18:24:45 +0300

    fix

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,16 @@
 export default (xml) => {
-    const channel = xml.querySelector('channel');
-    const title = channel.querySelector('title').textContent;
-    const description = channel.querySelector('description').textContent;
-    const items = channel.querySelectorAll('item');
-    const itemsList = [];
-    items.forEach((item) => {
-        const pubDate = new Date(item.querySelector('pubDate').textContent);
-        const titleItem = item.querySelector('title').textContent;
-        const linkItem = item.querySelector('link').textContent;
-        itemsList.push({ titleItem, linkItem, pubDate });
-    });
-    return { title, description, itemsList };
+  const domParser = new DOMParser();
+  const data = domParser.parseFromString(xml, 'text/xml');
+  const channel = data.querySelector('channel');
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const items = channel.querySelectorAll('item');
+  const itemsList = [];
+  items.forEach((item) => {
+    const pubDate = new Date(item.querySelector('pubDate').textContent);
+    const titleItem = item.querySelector('title').textContent;
+    const linkItem = item.querySelector('link').textContent;
+    itemsList.push({ titleItem, linkItem, pubDate });
+  });
+  return { title, description, itemsList };
 };
