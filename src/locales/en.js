47ccc9aef8584233aec2487e48c5561437f9e05f
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2020-04-04 12:44:37 +0300

    add traslationsfiles

---
+++ src/locales/en.js
@@ -0,0 +1,8 @@
+export default {
+  translation: {
+    network: 'Network Problems. Try again!',
+    dublicate: 'URL is present in rss flow!',
+    notValid: 'Value is not a valid url!',
+    empty: 'Input field should not to be empty',
+  },
+};
