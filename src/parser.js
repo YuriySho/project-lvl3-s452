6cba95185cc241e35369bf5ecc6c4488266293be
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2020-04-04 12:47:35 +0300

    fixed properties returned object

--- src/parser.js
+++ src/parser.js
@@ -2,15 +2,15 @@ export default (xml) => {
   const domParser = new DOMParser();
   const data = domParser.parseFromString(xml, 'text/xml');
   const channel = data.querySelector('channel');
-  const title = channel.querySelector('title').textContent;
+  const titleFeed = channel.querySelector('title').textContent;
   const description = channel.querySelector('description').textContent;
   const items = channel.querySelectorAll('item');
-  const itemsList = [];
+  const posts = [];
   items.forEach((item) => {
     const pubDate = new Date(item.querySelector('pubDate').textContent);
-    const titleItem = item.querySelector('title').textContent;
-    const linkItem = item.querySelector('link').textContent;
-    itemsList.push({ titleItem, linkItem, pubDate });
+    const title = item.querySelector('title').textContent;
+    const link = item.querySelector('link').textContent;
+    posts.push({ title, link, pubDate });
   });
-  return { title, description, itemsList };
+  return { titleFeed, description, posts };
 };
